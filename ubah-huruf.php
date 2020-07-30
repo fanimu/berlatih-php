216e355b626dbb3660714cd036348c0745abad73
Yepi Septiana <yepi.septiana@smartfren.com>
2020-07-30 08:10:49 +0700

    upload semua file jawaban

---
+++ ubah-huruf.php
@@ -0,0 +1,21 @@
+<?php
+function ubah_huruf($string){
+
+	$abjad = "abcdefghijklmnopqrstuvwxyz";
+	$output =" ";
+	for ($i=0; $i<strlen($string); $i++) { 
+		$position = strpos($abjad, $string[$i]);
+		$output .= substr($abjad, $position+1, 1);
+	}
+	return $output;
+
+}
+
+// TEST CASES
+echo ubah_huruf('wow')."<br>"; // xpx
+echo ubah_huruf('developer')."<br>"; // efwfmpqfs
+echo ubah_huruf('laravel')."<br>"; // mbsbwfm
+echo ubah_huruf('keren')."<br>"; // lfsfo
+echo ubah_huruf('semangat'); // tfnbohbu
+
+?>
